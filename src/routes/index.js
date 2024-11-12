1fff658184113f51d7fd5676aaa36b5532180eab
Facundo Gorlero <facundo.gorlero@hotmail.com>
2024-11-11 22:19:10 -0300

    faker afuera

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const cartRouter = require('./cart.route.js')
 const viewsRouter = require('./views.route.js')
 const sessionRouter = require('./session.route.js')
 const mailRouter = require('./mail.router.js')
-const pruebasRouter = require('./pruebas.router.js')
+
 const { handleError } = require('../midlewares/errors/handleerror.js')
 
 const router = Router()
@@ -15,7 +15,7 @@ router.use('/api/carts', cartRouter)
 router.use('/', viewsRouter)
 router.use('/api/session', sessionRouter)
 router.use('/api', mailRouter)
-router.use('/pruebas', pruebasRouter)
+
 //payment method
 
 
